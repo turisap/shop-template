29e03b60ae2fe17ff4035f5f8282d874eaeb1b5c
turisap <kirillshakirov57@gmail.com>
2018-11-11 21:06:43 +1300

    Prisma and youga completed

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,14 +1,17 @@
 const mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO Check if a user is logged in
-        const item  = await ctx.db.mutation.createItem({
-            data : {
-                ...args
-            }
-        }, info)
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if a user is logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-        return item;
-    }
+    return item;
+  }
 };
 
 module.exports = mutations;
