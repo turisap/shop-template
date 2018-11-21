e3d7a6d18c68482fae8e6fb12654c0e425784a26
turisap <kirillshakirov57@gmail.com>
2018-11-21 21:31:58 +1300

    Backend for items updates

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,17 +1,29 @@
 const mutations = {
-  async createItem(parent, args, ctx, info) {
-    // TODO Check if a user is logged in
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          ...args
-        }
-      },
-      info
-    );
+      async createItem(parent, args, ctx, info) {
+        // TODO Check if a user is logged in
+        const item = await ctx.db.mutation.createItem(
+          {
+            data: {
+              ...args
+            }
+          },
+          info
+        );
 
-    return item;
-  }
+        return item;
+      },
+
+      updateItem(parent, args, ctx, info) {
+          // get a copy of data for updates and revome id because we don't need to update it
+          const data = {...args};
+          delete data.id;
+          return ctx.db.mutation.updateItem({
+              data : updates,
+              where : {
+                  id : args.id
+              }
+          }, info)
+      }
 };
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   // this is for creating a query in a easy way by just forwarding it to the prisma.graphql
-  items: forwardTo("db")
+  items: forwardTo("db"),
+  item : forwardTo("db"),
   // async items (parent, args, ctx, info) {
   //     // console.log('getting items');
   //     // const item = await ctx.db.query.items();
