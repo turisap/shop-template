f7fc77a0752bee2d8006a3824690dde743a0ef11
turisap <kirillshakirov57@gmail.com>
2019-03-04 20:12:28 +1300

    Pagination queries

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from '../components/Item';
 import Pagination from './Pagination';
+import { CONFIG } from '../config';
 
 const Center = styled.div`
     text-align: center;
@@ -18,8 +19,8 @@ const ItemList = styled.div`
 `
 
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY ($skip : Int = 0, $first : Int = ${CONFIG.ITEMS_PER_PAGE}) {
+        items (first : $first, skip : $skip, orderBy : createdAt_DESC) {
             id
             title
             price
@@ -35,7 +36,12 @@ class Items extends Component {
             <Center>
                 <p>Items</p>
                 <Pagination page={this.props.page}/>
-                <Query query={ALL_ITEMS_QUERY}>
+                <Query
+                    query={ALL_ITEMS_QUERY}
+                    variables={{
+                        skip : (this.props.page - 1) * CONFIG.ITEMS_PER_PAGE
+                    }}
+                >
                     {({data, error, loading}) => {
                         if(loading) return <p>Loading..</p>
                         if(error) return <p>Error: {error.message}</p>
