b88e5e80ce8ce31d5cc617f9f4f858254710d77d
turisap <kirillshakirov57@gmail.com>
2019-04-18 22:37:39 +1200

    Snapshots for <Cart/>

---
+++ frontend/__tests__/Cart.test.js
@@ -0,0 +1,38 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import Cart, { LOCAL_STATE_QUERY } from '../components/Cart';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { CURRENT_USER_QUERY } from "../components/User";
+import {fakeUser, fakeCartItem} from "../lib/testUtils";
+
+const mocks = [
+    {
+        request : { query : CURRENT_USER_QUERY },
+        result : {
+            data : {
+                me : {
+                    ...fakeUser(),
+                    cart : [fakeCartItem()]
+                }
+            }
+        }
+    },
+    {
+        request : { query : LOCAL_STATE_QUERY },
+        result : { data : { cartOpen : true }}
+    }
+];
+
+describe('<Cart/>', () => {
+    it('renders and matches snappy', async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={mocks}>
+                <Cart/>
+            </MockedProvider>
+        );
+        await wait();
+        wrapper.update();
+        expect(toJSON(wrapper.find('header'))).toMatchSnapshot();
+    })
+})
